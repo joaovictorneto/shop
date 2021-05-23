dbd7f9b14aef08ef5ed4629ffe00ad58f41ce4a7
joaovictorneto <joaovictorneto@email.com>
2021-05-23 14:39:05 -0300

    Usando Provider e Extraindo Grid de Produtos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'package:shop/views/product_detail_screen.dart';
 import 'package:shop/views/products_overview_screen.dart';
@@ -8,15 +10,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minha Loja',
-      theme: ThemeData(
-        primarySwatch: Colors.lime,
-        accentColor: Colors.limeAccent,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (_) => new Products(),
+      child: MaterialApp(
+        title: 'Minha Loja',
+        theme: ThemeData(
+          primarySwatch: Colors.lime,
+          accentColor: Colors.limeAccent,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {AppRoutes.PRODUCT_DETAL: (context) => ProductDetailScreen()},
       ),
-      home: ProductOverviewScreen(),
-      routes: {AppRoutes.PRODUCT_DETAL: (context) => ProductDetailScreen()},
     );
   }
 }
